59938e3c5796e7a4b72a529b00c7439fbd3a61e1
shatong <843391622@qq.com>
2022-09-01 11:12:03 +0800

    feat:修改音符显示方式

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,9 @@ import { onMounted } from 'vue';
 import * as monaco from 'monaco-editor';
 import EditorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker';
 import Vex from "vexflow";
+import { parse } from "@encaik/abc";
+
+const { Renderer, Stave,StaveNote,Voice,Formatter } = Vex.Flow;
 
 self.MonacoEnvironment = {
   getWorker() {
@@ -11,19 +14,59 @@ self.MonacoEnvironment = {
   },
 }
 
+let scoreDefault = `X:1
+T:小鸡的一家
+M:2/4
+Q:1/4=140
+L:1/4
+K:C
+_C''3 =C,/2 ^C,,/4 ^^C,,,/8 __C,/8`;
+
 onMounted(() => {
   monaco.editor.create(document.getElementById('monaco'), {
-    value: "function hello() {\n\talert('Hello world!');\n}"
+    value: scoreDefault
   });
 
-let vf = new Vex.Flow.Factory({renderer: {elementId: 'score'}});
-let score = vf.EasyScore();
-let system = vf.System();
-system.addStave({
-  voices: [score.voice(score.notes('C#5/q, B4, A4, G#4'))]
-}).addClef('treble').addTimeSignature('4/4');
+  const durationMap = {
+    '4':'w',
+    '3':'hd',
+    '2':'h',
+    '1':'q',
+    '1/2':'8',
+    '1/4':'16',
+    '1/8':'32'
+  }
 
-vf.draw();
+  const div = document.getElementById("score");
+  const renderer = new Renderer(div, Renderer.Backends.SVG);
+  renderer.resize(div.clientWidth, div.clientHeight);
+  const context = renderer.getContext();
+  const stave = new Stave(10, 40, 400);
+  stave.addClef("treble").addTimeSignature("4/4");
+  stave.setContext(context).draw();
+  const notes = [
+    new StaveNote({ keys: ["c/4"], duration: "q" }),
+    new StaveNote({ keys: ["d/4"], duration: "q" }),
+    new StaveNote({ keys: ["b/4"], duration: "h" }),
+  ];
+
+  let transScore = parse(scoreDefault);
+  console.log(transScore);
+  let noteList = [];
+  for (let index = 0; index < transScore[0].music.length; index++) {
+    const note = transScore[0].music[index];
+    if(note.type==='note'){
+      noteList.push(new StaveNote({ keys: [`${note.pitch}/4`], duration: durationMap[note.duration] }));
+    }else{
+      continue;
+    }
+  }
+  console.log(noteList);
+
+  const voice = new Voice({ num_beats: 4, beat_value: 4 });
+  voice.addTickables(noteList);
+  new Formatter().joinVoices([voice]).format([voice], 350);
+  voice.draw(context, stave);
 })
 </script>
 
