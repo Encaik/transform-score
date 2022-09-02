c8686040eab7d774a7efca4893cd0d51c3feffa9
shatong <843391622@qq.com>
2022-09-02 10:26:12 +0800

    feat:添加abcjs版本页面

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <script setup>
-// import HelloWorld from './components/HelloWorld.vue'
-import { onMounted } from 'vue';
+import { ref,onMounted } from 'vue';
 import * as monaco from 'monaco-editor';
 import EditorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker';
 import Vex from "vexflow";
 import { parse } from "@encaik/abc";
+import ScoreAbc from './components/ScoreAbc.vue';
 
-const { Renderer, Stave,StaveNote,Voice,Formatter } = Vex.Flow;
+const { Renderer, Stave,StaveNote,Voice,Formatter,Accidental } = Vex.Flow;
 
 self.MonacoEnvironment = {
   getWorker() {
@@ -14,68 +14,83 @@ self.MonacoEnvironment = {
   },
 }
 
-let scoreDefault = `X:1
+let score = ref(`X:1
 T:小鸡的一家
 M:2/4
 Q:1/4=140
 L:1/4
 K:C
-_C''3 =C,/2 ^C,,/4 ^^C,,,/8 __C,/8`;
+_C''3 =C,/2 ^C,,/4 ^^C,,,/8 __C,/8`);
+let editor;
 
 onMounted(() => {
-  monaco.editor.create(document.getElementById('monaco'), {
-    value: scoreDefault
+  editor = monaco.editor.create(document.getElementById('monaco'), {
+    value: score.value
   });
 
-  const durationMap = {
-    '4':'w',
-    '3':'hd',
-    '2':'h',
-    '1':'q',
-    '1/2':'8',
-    '1/4':'16',
-    '1/8':'32'
-  }
+  editor.onDidChangeModelContent(()=>{
+    score.value = editor.getValue();
+    console.log(score.value);
+  })
 
-  const div = document.getElementById("score");
-  const renderer = new Renderer(div, Renderer.Backends.SVG);
-  renderer.resize(div.clientWidth, div.clientHeight);
-  const context = renderer.getContext();
-  const stave = new Stave(10, 40, 400);
-  stave.addClef("treble").addTimeSignature("4/4");
-  stave.setContext(context).draw();
-  const notes = [
-    new StaveNote({ keys: ["c/4"], duration: "q" }),
-    new StaveNote({ keys: ["d/4"], duration: "q" }),
-    new StaveNote({ keys: ["b/4"], duration: "h" }),
-  ];
+  // const durationMap = {
+  //   '4':'w',
+  //   '3':'hd',
+  //   '2':'h',
+  //   '1':'q',
+  //   '/2':'8',
+  //   '/4':'16',
+  //   '/8':'32'
+  // }
 
-  let transScore = parse(scoreDefault);
-  console.log(transScore);
-  let noteList = [];
-  for (let index = 0; index < transScore[0].music.length; index++) {
-    const note = transScore[0].music[index];
-    if(note.type==='note'){
-      noteList.push(new StaveNote({ keys: [`${note.pitch}/4`], duration: durationMap[note.duration] }));
-    }else{
-      continue;
-    }
-  }
-  console.log(noteList);
+  // const div = document.getElementById("score");
+  // const renderer = new Renderer(div, Renderer.Backends.SVG);
+  // renderer.resize(div.clientWidth, div.clientHeight);
+  // const context = renderer.getContext();
+  // const stave = new Stave(50, 50, div.clientWidth-100);
+  // stave.addClef("treble").addTimeSignature("4/4");
+  // stave.setContext(context).draw();
 
-  const voice = new Voice({ num_beats: 4, beat_value: 4 });
-  voice.addTickables(noteList);
-  new Formatter().joinVoices([voice]).format([voice], 350);
-  voice.draw(context, stave);
+  // let transScore = parse(scoreDefault);
+  // console.log(transScore);
+  // let noteList = [];
+  // for (let index = 0; index < transScore[0].music.length; index++) {
+  //   const note = transScore[0].music[index];
+  //   if(note.type==='note'){
+  //     let staveNote = new StaveNote({ keys: [`${note.name}/${note.pitch||4}`], duration: durationMap[note.duration] });
+  //     if(note.sharp){
+  //       staveNote.addModifier(new Accidental("#"))
+  //     }
+  //     if(note.flat){
+  //       staveNote.addModifier(new Accidental("b"))
+  //     }
+  //     if(note['double-sharp']){
+  //       staveNote.addModifier(new Accidental("##"))
+  //     }
+  //     if(note['double-flat']){
+  //       staveNote.addModifier(new Accidental("bb"))
+  //     }
+  //     noteList.push(staveNote);
+  //   }else{
+  //     continue;
+  //   }
+  // }
+  // console.log(noteList);
+
+  // const voice = new Voice({ num_beats: 4, beat_value: 4 });
+  // voice.addTickables(noteList);
+  // new Formatter().joinVoices([voice]).format([voice], 350);
+  // voice.draw(context, stave);
 })
 </script>
 
 <template>
   <div class="flex h-full w-full">
     <div id="monaco" class="w-6/12"></div>
-    <div id="score" class="w-6/12"></div>
+    <div class="flex flex-col w-6/12">
+      <ScoreAbc class="flex-1" v-model:score="score"></ScoreAbc>
+    </div>
   </div>
-  <!-- <HelloWorld msg="Vite + Vue" /> -->
 </template>
 
 <style scoped>
---
+++ src/components/ScoreAbc.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { defineProps,onMounted,watch,ref } from "vue";
+import abcjs from "abcjs";
+
+let props = defineProps({
+  score: String
+});
+
+let score = ref(props.score);
+let container;
+
+watch(
+  () => props.score,
+  (value) => {
+    score.value = value;
+    abcjs.renderAbc("score", score.value,{staffwidth:container.clientWidth-30});
+  }
+)
+
+onMounted(()=>{
+  container = document.getElementById("score");
+  abcjs.renderAbc("score", score.value,{staffwidth:container.clientWidth-30});
+})
+</script>
+
+<template>
+  <div id="score"></div>
+</template>
+
+<style scoped>
+</style>
